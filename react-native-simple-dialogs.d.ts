6a77235a7a906fd4ad6e1a440f33d89b00fa0d55
aarsenev <alexey.arsenev@tularegion.ru>
2020-06-06 15:46:58 +0300

    Update typings based on ammichael's gist

--- react-native-simple-dialogs.d.ts
+++ react-native-simple-dialogs.d.ts
@@ -1,59 +1,70 @@
-import * as React from 'react';
-import { View, ViewProps,Text,Button,StyleProp,Modal,ModalProps,ButtonProps } from 'react-native';
-
-
-declare module "react-native-simple-dialogs" {
+declare module 'react-native-simple-dialogs' {
+    import * as React from 'react';
+    import {
+        View,
+        ViewProps,
+        Text,
+        Button,
+        StyleProp,
+        ButtonProps,
+    } from 'react-native';
 
     type animationType = 'none' | 'slide' | 'fade';
 
-    type supportedOrientationsType   = 'portrait' |'portrait-upside-down'| 'landscape'|'landscape-left'| 'landscape-right';
+    type supportedOrientationsType =
+      | 'portrait'
+      | 'portrait-upside-down'
+      | 'landscape'
+      | 'landscape-left'
+      | 'landscape-right';
 
-    type keyboardDismissModeType=  'none'| 'on-drag'| 'interactive';
-    
-    type keyboardShouldPersistTapsType= 'always' | 'never' | 'handled' | false | true;
+    type keyboardDismissModeType = 'none' | 'on-drag' | 'interactive';
 
-    type activityIndicatorSizeType= 'small' | 'large';
+    type keyboardShouldPersistTapsType =
+      | 'always'
+      | 'never'
+      | 'handled'
+      | false
+      | true;
+
+    type activityIndicatorSizeType = 'small' | 'large';
 
     export interface DialogProps {
-
-        visible?: boolean,
-        onRequestClose?: ()=>void,
-        animationType?:animationType,
-        onShow?: ()=>void,
-        onOrientationChange?:()=>void,
-        supportedOrientations?:supportedOrientationsType,
-        onTouchOutside?:()=>void,
-        title?: string,
-        titleStyle?: StyleProp<Text>,
-        dialogStyle?: StyleProp<View>,
-        contentStyle?:  ViewProps<View>,
-        buttonsStyle?:  StyleProp<View>,
-        overlayStyle?:  StyleProp<View>,
-        buttons?: ButtonProps<Button>,
-        keyboardDismissMode?: keyboardDismissModeType,
-        keyboardShouldPersistTaps?: keyboardShouldPersistTapsType,
+        visible?: boolean;
+        onRequestClose?: () => void;
+        animationType?: animationType;
+        onShow?: () => void;
+        onOrientationChange?: () => void;
+        supportedOrientations?: supportedOrientationsType;
+        onTouchOutside?: () => void;
+        title?: string;
+        titleStyle?: StyleProp<Text>;
+        dialogStyle?: StyleProp<View>;
+        contentStyle?: ViewProps<View>;
+        buttonsStyle?: StyleProp<View>;
+        overlayStyle?: StyleProp<View>;
+        buttons?: ButtonProps<Button>;
+        keyboardDismissMode?: keyboardDismissModeType;
+        keyboardShouldPersistTaps?: keyboardShouldPersistTapsType;
     }
 
-    export interface ProgressDialogProps extends DialogProps{
-    message: string,
-    messageStyle?:StyleProp<Text>,
-    activityIndicatorColor?: string,
-    activityIndicatorSize?: activityIndicatorSizeType,
-    activityIndicatorStyle?:StyleProp<View>
+    export interface ProgressDialogProps extends DialogProps {
+        message: string;
+        messageStyle?: StyleProp<Text>;
+        activityIndicatorColor?: string;
+        activityIndicatorSize?: activityIndicatorSizeType;
+        activityIndicatorStyle?: StyleProp<View>;
     }
 
-    export interface ConfirmDialogProps extends DialogProps{
-    message?: string,
-    messageStyle?: StyleProp<Text>,
-    negativeButton?: ButtonProps<Button>,
-    positiveButton: ButtonProps<Button>
-    } 
+    export interface ConfirmDialogProps extends DialogProps {
+        message?: string;
+        messageStyle?: StyleProp<Text>;
+        negativeButton?: ButtonProps<Button>;
+        positiveButton: ButtonProps<Button>;
+    }
 
-
-    class Dialog extends React.Component<DialogProps>{}
-    class ProgressDialog extends React.Component<ProgressDialogProps>{}
-    class ConfirmDialog  extends React.Component <ConfirmDialogProps>{}
-    class TouchableEffect extends React.Component <Props>{}
+    export class Dialog extends React.Component<DialogProps> {}
+    export class ProgressDialog extends React.Component<ProgressDialogProps> {}
+    export class ConfirmDialog extends React.Component<ConfirmDialogProps> {}
+    export class TouchableEffect extends React.Component<Props> {}
 }
-
-export {Dialog, ProgressDialog, ConfirmDialog,TouchableEffect }
